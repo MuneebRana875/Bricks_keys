55c0374d697f5beb17bbd3f2edb28bd67aab3079
sundasfatima476 <sundasfatima476@gmail.com>
2026-03-27 14:56:41 +0500

    Updated FIndHomeCTA

--- bricks/src/components/FindHomeCTA.jsx
+++ bricks/src/components/FindHomeCTA.jsx
@@ -4,16 +4,15 @@ import { Link } from "react-router-dom";
 
 // Images Import
 import home1 from "../assets/images/h47.png.png"; 
-import home2 from "../assets/images/h48.png.png";
 
 const FindHomeCTA = () => {
   return (
     <section style={{ padding: "80px 0", background: "#fdf7f5" }}>
       <div className="container">
-        <div className="row g-4">
+        <div className="row justify-content-center">
 
           {/* Card 1: Looking for new home */}
-          <div className="col-lg-6">
+          <div className="col-lg-10">
             <div
               style={{
                 background: "#F2F2F2", 
@@ -75,74 +74,6 @@ const FindHomeCTA = () => {
                   height: "auto", 
                   objectFit: "contain",
                   marginBottom: "-20px" 
-                }}
-              />
-            </div>
-          </div>
-
-          {/* Card 2: Want to sell home */}
-          <div className="col-lg-6">
-            <div
-              style={{
-                background: "#F2E7E3", 
-                padding: "40px",
-                borderRadius: "20px",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                height: "100%",
-                overflow: "hidden"
-              }}
-            >
-              <div style={{ flex: 1, zIndex: 1 }}>
-                <h4 style={{ fontWeight: "700", marginBottom: "15px", color: "#1A1A1A" }}>
-                  Want to sell <br /> your home?
-                </h4>
-
-                <p
-                  style={{
-                    fontSize: "14px",
-                    color: "#666",
-                    lineHeight: "1.6",
-                    marginBottom: "25px",
-                    maxWidth: "250px"
-                  }}
-                >
-                  10 new offers every day. 350 offers on site, trusted by
-                  a community of thousands of users.
-                </p>
-
-                <Link
-                  to="/get-started"
-                  className="text-decoration-none"
-                  style={{
-                    background: "#1A432F",
-                    color: "#fff",
-                    padding: "12px 24px",
-                    borderRadius: "10px",
-                    display: "inline-flex",
-                    alignItems: "center",
-                    gap: "10px",
-                    fontWeight: "600",
-                    transition: "0.3s",
-                    border: "none"
-                  }}
-                  onMouseOver={(e) => (e.currentTarget.style.opacity = "0.9")}
-                  onMouseOut={(e) => (e.currentTarget.style.opacity = "1")}
-                >
-                  Get Started <FaArrowRight size={14} />
-                </Link>
-              </div>
-
-              {/* Illustration 2 */}
-              <img
-                src={home2}
-                alt="Sell Home"
-                style={{ 
-                  width: "180px", 
-                  height: "auto", 
-                  objectFit: "contain",
-                  marginBottom: "-20px"
                 }}
               />
             </div>
